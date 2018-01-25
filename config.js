bd01085b21244d61b173079fdc951419c96a0bac
Trần Đức Thái <tranducthai8@gmail.com>
2018-01-26 00:59:10 +0700

    config project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    secret: 'jsonwebtoken',
+    // url : 'mongodb://localhost/ifood-final'
+    url:'mongodb://polo33333:12345678.c@ds135537.mlab.com:35537/ifood'
+  };
---
+++ server.js
@@ -0,0 +1,73 @@
+const express           = require('express');
+const bodyParser        = require('body-parser');
+const path              = require('path');
+const http              = require('http');
+const jwt               = require('jsonwebtoken');
+const app               = express();
+const mongoose          = require('mongoose');  
+const config            = require('./config');
+
+
+// connect to MongoDb
+var mongoDBURL = config.url;
+// var options = {
+//     useMongoClient: true,
+// }
+
+mongoose.connect(mongoDBURL);
+mongoose.Promise = global.Promise;
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.set('superSecret', config.secret);
+// API file for interacting with MongoDB
+const api = require('./server/routes/api');
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+// Add headers
+app.use(function (req, res, next) {
+    
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+    
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
+    });
+
+// API location
+app.use('/api', api);
+
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Server is running on localhost:${port}`));
